9579c070185e71281af64ce0eb92a102a54b68d4
dheapuspita <90955237+dheapuspita@users.noreply.github.com>
2022-10-10 23:38:47 +0800

    posttest 4

---
+++ artsyde/register.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../artsyde/Pictures/frame.png">
+    <link rel="stylesheet" href="relostyle.css">
+    <title>Register</title>
+</head>
+<body>
+    <div class="container">
+      <form action="" method="POST" class="login-email">
+        <p style="font-size: 2rem; font-weight: 850;">REGISTER</p>
+        <div class="input-group">
+            <input type="text" placeholder="Full Name" name="fullname">
+        </div>
+        <div class="input-group">
+            <input type="number" placeholder="Age" name="age">
+        </div>
+        <div class="input-group">
+            <input type="text" placeholder="Email" name="email">
+        </div>
+        <div class="input-group">
+            <input type="password" placeholder="Password" name="password">
+        </div>
+        <div class="input-group">
+            <input type="text" placeholder="Phone Number" name="number">
+        </div>
+        <div class="input-group">
+            <input type="password" placeholder="Confirm Password" name="cfpassword">
+        </div>
+        <div class="input-group">
+            <button name="submit" class="btn">Register Now</button>
+        </div>
+        <p class="login-register">Have an account?
+            <a href="index.php">Login</a>
+        </p>
+      </form>  
+    </div>
+</body>
+</html>
